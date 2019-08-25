80f228c389fe83788ae556fd00f19eff8c75ed0d
Tanay Patankar <tanaypatankar.2000@gmail.com>
2019-08-25 14:19:07 +0530

    Somewhat works. Keywords very little

---
+++ tcg_1.py
@@ -0,0 +1,26 @@
+def squeeze(tos, bad_phrases):
+    getsentences(tos, bad_phrases)
+
+
+def getsentences(tos, bad_phrases):
+    f = open(tos, encoding="utf8")
+    sentences = f.read().lower().split('.')
+    searchphrases(sentences, bad_phrases)
+
+
+def searchphrases(sentences, bad_phrases):
+    f = open(bad_phrases,  encoding="utf8")
+    squeezed = []
+    phrase = f.read().lower().splitlines()
+    for sentence in sentences:
+        word = sentence.split()
+        for w in word:
+            if w in phrase and sentence not in squeezed:
+                squeezed.append(sentence)
+    for s in squeezed:
+        print(s)
+
+
+squeeze('rovio_tos.txt', 'bad_phrases.txt')
+print("\n\n\n\n\n\n\nThis is the eula stuff\n\n\n\n\n\n\n")
+squeeze('rovio_eula.txt', 'bad_phrases.txt')
